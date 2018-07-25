0c9d22526ce3ca98ff0139a3e3ee73f6df081e13
grdaimap <36882078+grdaimap@users.noreply.github.com>
2018-07-25 14:28:46 +0800

    Add files via upload

--- tfmnistcnn/tfres恢复参数到应用模型.py
+++ tfmnistcnn/tfres恢复参数到应用模型.py
@@ -6,6 +6,11 @@ import matplotlib.pyplot as plt
 tf.reset_default_graph()
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
+# Define the model (same as when creating the model file)
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
 
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
