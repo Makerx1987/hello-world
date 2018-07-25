0149edae3cb930516fc25e56ca1890d9dc9243cc
grdaimap <36882078+grdaimap@users.noreply.github.com>
2018-07-25 16:02:59 +0800

    【重置】

--- tfmnistcnn/tfmnistcnn.py
+++ tfmnistcnn/tfmnistcnn.py
@@ -6,10 +6,30 @@ import tensorflow as tf
 
 sess = tf.InteractiveSession()
 
+
 x = tf.placeholder(tf.float32, shape=[None, 784])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
+
+
+sess.run(tf.global_variables_initializer())
+
+y = tf.matmul(x, W) + b
+
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+for _ in range(1000):
+    batch = mnist.train.next_batch(100)
+    train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
 
 def weight_variable(shape):
@@ -60,18 +80,19 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+# saver = tf.train.Saver()  # defaults to saving all variables
 
 sess.run(tf.global_variables_initializer())
-
-for i in range(30000):
+for i in range(20000):
     batch = mnist.train.next_batch(50)
-    if (i<1000 and i%10==0)or(i<10000 and i%100 == 0)or i%1000 == 0:
+    if i % 100 == 0:
         train_accuracy = accuracy.eval(feed_dict={
             x: batch[0], y_: batch[1], keep_prob: 1.0})
         print("step %d, training accuracy %g" % (i, train_accuracy))
 
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
+# saver.save(sess, './model.ckpt')
+# #保存模型参数，注意把这里改为自己的路径
 
 print("test accuracy %g" % accuracy.eval(feed_dict={
     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
